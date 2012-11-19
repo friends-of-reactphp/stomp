cbe6b0b349df6d0413ba465e7872c98b2d068e0e
Romain Neutron <imprec@gmail.com>
2012-11-19 13:14:06 +0100

    Use connect event to set up heart beat frames

--- src/React/Stomp/Client/Command/ConnectionEstablishedCommand.php
+++ src/React/Stomp/Client/Command/ConnectionEstablishedCommand.php
@@ -2,14 +2,16 @@
 
 namespace React\Stomp\Client\Command;
 
+use React\Stomp\Protocol\Frame;
+
 // TODO: this is weird, should be an event instead of command
 
 class ConnectionEstablishedCommand implements CommandInterface
 {
-    public $heartbeatServerSettings;
+    public $frame;
 
-    public function __construct($heartbeatServerSettings)
+    public function __construct(Frame $frame)
     {
-        $this->heartbeatServerSettings = $heartbeatServerSettings;
+        $this->frame = $frame;
     }
 }
