09ee73fff428c1508471a143293586205ce23833
Romain Neutron <imprec@gmail.com>
2012-12-27 15:58:14 +0100

    Probe enhancement

--- examples/config/probe.php
+++ examples/config/probe.php
@@ -4,8 +4,8 @@ if (!isset($argv[1]) || !file_exists(__DIR__ . '/' . $argv[1] . '.php')) {
     $current = isset($argv[1]) ? $argv[1] : null;
     $configs = array();
 
-    foreach (new DirectoryIterator(__DIR__) as $fileInfo) {
-        if (basename(__FILE__) === $fileInfo->getFilename() || $fileInfo->isDot()) {
+    foreach (new FilesystemIterator(__DIR__) as $fileInfo) {
+        if (basename(__FILE__) === $fileInfo->getFilename()) {
             continue;
         }
 
@@ -18,7 +18,8 @@ if (!isset($argv[1]) || !file_exists(__DIR__ . '/' . $argv[1] . '.php')) {
         $current, implode(', ', $configs)
     );
 
-    exit("\nEx : /usr/bin/php stomp-basic.php rabbitmq\n\n");
+    echo("\nEx : /usr/bin/php stomp-basic.php rabbitmq\n\n");
+    exit(1);
 }
 
 return require __DIR__ . '/' . $argv[1] . '.php';
