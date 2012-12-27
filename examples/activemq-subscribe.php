110f308508e86b2f61bcf916386d842c33e648c3
Romain Neutron <imprec@gmail.com>
2012-12-27 13:43:53 +0100

    Port rabbit-mq scripts to active-mq

---
+++ examples/activemq-subscribe.php
@@ -0,0 +1,30 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+$loop = React\EventLoop\Factory::create();
+$factory = new React\Stomp\Factory($loop);
+$client = $factory->createClient(array(
+    'host'      => 'localhost',
+    'login'     => 'system',
+    'passcode'  => 'manager',
+));
+
+$client
+    ->connect()
+    ->then(function ($client) use ($loop) {
+        $prevMessageCount = 0;
+        $messageCount = 0;
+
+        $client->subscribe('/topic/foo', function ($frame) use (&$messageCount) {
+            $messageCount++;
+        });
+
+        $loop->addPeriodicTimer(1, function () use (&$prevMessageCount, &$messageCount) {
+            $diff = $messageCount - $prevMessageCount;
+            echo "Received this second: $diff\n";
+            $prevMessageCount = $messageCount;
+        });
+    });
+
+$loop->run();
