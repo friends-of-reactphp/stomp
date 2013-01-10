4f2b6000a4e53b480e57af00e0fd60e88ac71937
Romain Neutron <imprec@gmail.com>
2013-01-10 14:43:41 +0100

    Move functionnal configuration to PHPUnit script

--- tests/React/FunctionalTests/Stomp/FunctionalTestCase.php
+++ tests/React/FunctionalTests/Stomp/FunctionalTestCase.php
@@ -7,6 +7,26 @@ use React\Stomp\Factory;
 
 abstract class FunctionalTestCase extends \PHPUnit_Framework_TestCase
 {
+    private static $loaded = false;
+
+    public static function setUpBeforeClass()
+    {
+        parent::setUpBeforeClass();
+
+        if (!self::$loaded) {
+            self::load();
+        }
+    }
+
+    public static function tearDownAfterClass()
+    {
+        if (self::$loaded) {
+            self::unload();
+        }
+
+        parent::tearDownAfterClass();
+    }
+
     protected function getEventLoop()
     {
         return LoopFactory::create();
@@ -24,5 +44,31 @@ abstract class FunctionalTestCase extends \PHPUnit_Framework_TestCase
         $options = array_merge($default, $options);
 
         return $factory->createClient($options);
+    }
+
+    private static function load()
+    {
+        if (false === $provider = getenv('STOMP_PROVIDER')) {
+            throw new \RuntimeException('STOMP_PROVIDER environment variable is not set');
+        }
+
+        $source = __DIR__ . '/../../../../examples/config/' . $provider . '.php';
+
+        if (!file_exists($source)) {
+            throw new \RuntimeException(sprintf('%s is not an available provider', $provider));
+        }
+
+        copy($source, __DIR__ . '/../config.php');
+
+        self::$loaded = true;
+    }
+
+    private static function unload()
+    {
+        if (file_exists(__DIR__ . '/../config.php')) {
+            unlink(__DIR__ . '/../config.php');
+        }
+
+        self::$loaded = false;
     }
 }
