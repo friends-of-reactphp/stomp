ded4398732874704370f17c2065325d3905e51e6
Igor Wiedler <igor@wiedler.ch>
2013-01-15 02:07:41 +0100

    Remove crazy config file copying from functional test case

--- tests/React/FunctionalTests/Stomp/FunctionalTestCase.php
+++ tests/React/FunctionalTests/Stomp/FunctionalTestCase.php
@@ -7,26 +7,6 @@ use React\Stomp\Factory;
 
 abstract class FunctionalTestCase extends \PHPUnit_Framework_TestCase
 {
-    private static $loaded = false;
-
-    public static function setUpBeforeClass()
-    {
-        parent::setUpBeforeClass();
-
-        if (!self::$loaded) {
-            self::load();
-        }
-    }
-
-    public static function tearDownAfterClass()
-    {
-        if (self::$loaded) {
-            self::unload();
-        }
-
-        parent::tearDownAfterClass();
-    }
-
     protected function getEventLoop()
     {
         return LoopFactory::create();
@@ -36,39 +16,20 @@ abstract class FunctionalTestCase extends \PHPUnit_Framework_TestCase
     {
         $factory = new Factory($loop);
 
-        if (!file_exists(__DIR__ . '/../config.php')) {
-            $this->markTestSkipped(sprintf('No config file found at %s, skipping', realpath(__DIR__ . '/../config.php')));
-        }
-
-        $default = require __DIR__ . '/../config.php';
-        $options = array_merge($default, $options);
-
-        return $factory->createClient($options);
-    }
-
-    private static function load()
-    {
-        if (false === $provider = getenv('STOMP_PROVIDER')) {
+        if (false === getenv('STOMP_PROVIDER')) {
             throw new \RuntimeException('STOMP_PROVIDER environment variable is not set');
         }
 
-        $source = __DIR__ . '/../../../../examples/config/' . $provider . '.php';
+        $provider = getenv('STOMP_PROVIDER');
+        $configFile = __DIR__.'/../../../../examples/config/'.$provider.'.php';
 
-        if (!file_exists($source)) {
-            throw new \RuntimeException(sprintf('%s is not an available provider', $provider));
+        if (!file_exists($configFile)) {
+            $this->markTestSkipped(sprintf('Invalid STOMP_PROVIDER: No config file found at %s', realpath($configFile)));
         }
 
-        copy($source, __DIR__ . '/../config.php');
+        $default = require $configFile;
+        $options = array_merge($default, $options);
 
-        self::$loaded = true;
-    }
-
-    private static function unload()
-    {
-        if (file_exists(__DIR__ . '/../config.php')) {
-            unlink(__DIR__ . '/../config.php');
-        }
-
-        self::$loaded = false;
+        return $factory->createClient($options);
     }
 }
