6fe3fb61569d491720e6c69fae3f44a473934b87
Alex Salt <alex.salt@e96.ru>
2014-08-05 17:35:50 +0600

    fix OutputStreamInterface

--- src/React/Stomp/Io/OutputStreamInterface.php
+++ src/React/Stomp/Io/OutputStreamInterface.php
@@ -3,7 +3,6 @@
 namespace React\Stomp\Io;
 
 use React\Stomp\Protocol\Frame;
-use React\Stream\StreamInterface;
 
 /**
  * PHP <= 5.3.8 does not support a close method in this interface.
@@ -11,7 +10,7 @@ use React\Stream\StreamInterface;
  * OutputStream is extending ReadableStream and implements OutputStreamInterface
  * that both have the `close` method in common.
  */
-interface OutputStreamInterface extends StreamInterface
+interface OutputStreamInterface extends \React\Stream\ReadableStreamInterface
 {
     public function sendFrame(Frame $frame);
 }
